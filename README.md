9f04d1eef8163d44519f8d13dc5efd3efb0c7a8e
Márcio Prando Teixeira <mprandot@gmail.com>
2020-08-09 22:56:58 -0300

    #MC: Convert values on typing
    - Add container to separate icon from first input

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Repository with many projects using flutter.
 
 ## Money conversion 
 
-Convert money from different currencies.
+- Use hgbrasil finances API to convert currencies.
+- Use FutureBuilder to make a loading and render widgets
+- Implement .env config type
+- Use functions to generate Widgets
+
--- money_conversion/lib/main.dart
+++ money_conversion/lib/main.dart
@@ -32,6 +32,29 @@ class _HomeState extends State<Home> {
   double dolar = 0.0;
   double euro = 0.0;
 
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+  
+
+  void realChange(String text) {
+    double real = double.parse(text);
+    dolarController.text = (real/dolar).toStringAsFixed(2);   
+    euroController.text = (real/euro).toStringAsFixed(2);
+  }
+
+  void dolarChange(String text) {
+    double _dolar  = double.parse(text);
+    realController.text = (_dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (_dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void euroChange(String text) {
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);   
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,26 +81,24 @@ class _HomeState extends State<Home> {
                       style: TextStyle(color: Colors.amber, fontSize: 25.0),
                       textAlign: TextAlign.center));
               } else {
+                
                 dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
                 euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
                 
-                return 
-                SingleChildScrollView(
+                return SingleChildScrollView(
                   padding: EdgeInsets.all(20.0),
                   child: Column( 
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Icon(Icons.monetization_on, size: 150.0, color: Colors.amber),
-
-                      buildTextField("Reais", "R\$"),
-
+                      Container(
+                        padding: EdgeInsets.only(bottom: 10.0),
+                        child: Icon(Icons.monetization_on, size: 150.0, color: Colors.amber),
+                      ),
+                      buildTextField("Reais", "R\$", realController, realChange),
                       Divider(),
-                      
-                      buildTextField("Dólar", "US\$"),
-
+                      buildTextField("Dólar", "US\$", dolarController, dolarChange),
                       Divider(),
-                      
-                      buildTextField("Euro", "€")
+                      buildTextField("Euro", "€", euroController, euroChange)
                     ]
                   )
                 );
@@ -89,8 +110,10 @@ class _HomeState extends State<Home> {
   }
 }
 
-Widget buildTextField(String label, String prefix) {
+Widget buildTextField(String label, String prefix, TextEditingController _controller, Function onChange) {
   return TextField(
+    keyboardType: TextInputType.number,
+    controller: _controller,
     style: TextStyle(
       color: Colors.amber,
       fontSize: 25.0
@@ -103,6 +126,7 @@ Widget buildTextField(String label, String prefix) {
         color: Colors.amber
       )
     ),
+    onChanged: onChange,
   );
 }
 
