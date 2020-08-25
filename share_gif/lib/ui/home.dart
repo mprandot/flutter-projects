aeb1bf059179aea4bcba45136bb28630a2241e91
Márcio Prando Teixeira <mprandot@gmail.com>
2020-08-24 22:05:07 -0300

    #SG: Add Future builder to show loading  and GridView

--- share_gif/lib/ui/home.dart
+++ share_gif/lib/ui/home.dart
@@ -32,9 +32,9 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
 
-    _getGifs().then((response) {
-      print(response);
-    });
+    // _getGifs().then((response) {
+    //   print(response);
+    // });
   }
 
   @override
@@ -58,9 +58,49 @@ class _HomeState extends State<Home> {
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
             ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+                future: _getGifs(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        width: 200.0,
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5.0,
+                        ),
+                      );
+                    default:
+                      if (snapshot.hasError)
+                        return Container();
+                      else
+                        return _createGiftTable(context, snapshot);
+                  }
+                }),
           )
         ],
       ),
     );
+  }
+
+  Widget _createGiftTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: snapshot.data["data"].length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+              child: Image.network(
+                  snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+                  height: 300.0,
+                  fit: BoxFit.cover));
+        });
   }
 }
